b5dbc54eed9919263a6005582364f43c5c392557
Jp <jyotiprakashpc@outlook.com>
2022-09-16 20:40:26 +0530

    average rating

---
+++ UC10Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductReviewSystem
+{
+    public class UC10Program
+    {
+        public static void Main()
+        {
+            UC10ProductHandler.CreateAndInsertToDatabase();
+            UC10ProductHandler.GetAverageRating();
+        }
+    }
+}
