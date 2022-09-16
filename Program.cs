48e730368fa797b62b1addc8f7465129543e8277
Jp <jyotiprakashpc@outlook.com>
2022-09-16 20:18:03 +0530

    Welcome code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewSystem
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Product Review System....");
+        }
+    }
+}
