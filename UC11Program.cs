8ad0c52d793fd74483efd4037bd529e848c7cd7a
Jp <jyotiprakashpc@outlook.com>
2022-09-16 20:42:44 +0530

    retrieve all records with review nice

---
+++ UC11Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductReviewSystem
+{
+    public class UC11Program
+    {
+        public static void Main()
+        {
+            UC11ProductHandler.CreateAndInsertToDatabase();
+            UC11ProductHandler.GetRecordsReviewNice();
+        }
+    }
+}
