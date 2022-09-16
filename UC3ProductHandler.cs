b01a60c3238aa3ca658ad1caff595b62145a21db
Jp <jyotiprakashpc@outlook.com>
2022-09-16 20:27:02 +0530

    Retrieve all rating greater than 3

---
+++ UC3ProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+
+namespace ProductReviewSystem
+{
+    public static class UC3ProductHandler
+    {
+        public static List<UC3ProductReview> productList = new List<UC3ProductReview>();
+        public static List<UC3ProductReview> ListOfRecords()
+        {
+            productList.Add(new UC3ProductReview(1, 1, 5, "good", false));
+            productList.Add(new UC3ProductReview(2, 2, 2, "good", false));
+            productList.Add(new UC3ProductReview(7, 4, 3, "good", true));
+            productList.Add(new UC3ProductReview(4, 2, 1, "bad", false));
+            productList.Add(new UC3ProductReview(1, 1, 3.5, "good", true));
+            productList.Add(new UC3ProductReview(2, 9, 3, "good", true));
+            productList.Add(new UC3ProductReview(3, 7, 9.5, "good", true));
+            productList.Add(new UC3ProductReview(7, 4, 4, "good", true));
+            productList.Add(new UC3ProductReview(9, 0, 3, "good", true));
+            return productList;
+        }
+        public static void GetRecordBasedOnRating()
+        {
+            var selectedRecords = from ProductReview in ListOfRecords()
+                                  where (ProductReview.ProductID == 1 && ProductReview.Ratings > 3) ||
+                                    (ProductReview.ProductID == 4 && ProductReview.Ratings > 3) ||
+                                    (ProductReview.ProductID == 9 && ProductReview.Ratings > 3)
+                                  select ProductReview;
+            foreach (UC3ProductReview records in selectedRecords)
+            {
+                Console.WriteLine("Product: {0},{1},{2},{3},{4}", records.ProductID, records.UserID, records.Ratings, records.Review, records.IsLike);
+            }
+        }
+    }
+}
---
+++ UC3Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductReviewSystem
+{
+    public class UC3Program
+    {
+        public static void Main()
+        {
+            UC3ProductHandler.ListOfRecords();
+            UC3ProductHandler.GetRecordBasedOnRating();
+        }
+    }
+}
