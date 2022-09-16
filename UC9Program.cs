cf492b6916da94002f9f4fc5e1820f012c4f3888
Jp <jyotiprakashpc@outlook.com>
2022-09-16 20:39:21 +0530

    Retrieve all from data table

---
+++ UC9Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductReviewSystem
+{
+    public class UC9Program
+    {
+        public static void Main()
+        {
+            UC9ProductHandler.CreateAndInsertToDatabase();
+            UC9ProductHandler.GetRecordsValueIsLike();
+        }
+    }
+}
