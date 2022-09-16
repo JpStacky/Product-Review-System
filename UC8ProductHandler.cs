370a81bbc7c7ad3a273f8121b2c46c86b50f4770
Jp <jyotiprakashpc@outlook.com>
2022-09-16 20:38:00 +0530

    create data table

---
+++ UC8ProductHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+
+namespace ProductReviewSystem
+{
+    public static class UC8ProductHandler
+    {
+        public static DataTable dataTable = new DataTable();
+        public static string CreateAndInsertToDatabase()
+        {
+            dataTable.Columns.Add("ProductId");
+            dataTable.Columns.Add("UserId");
+            dataTable.Columns.Add("Rating");
+            dataTable.Columns.Add("Review");
+            dataTable.Columns.Add("IsLike");
+            dataTable.Rows.Add(1, 1, 5, "bad", false);
+            dataTable.Rows.Add(2, 2, 5, "good", true);
+            dataTable.Rows.Add(3, 3, 4, "better", true);
+            dataTable.Rows.Add(4, 4, 3, "good", false);
+            dataTable.Rows.Add(5, 5, 5, "best", true);
+            dataTable.Rows.Add(6, 6, 4, "good", true);
+            dataTable.Rows.Add(7, 7, 3, "good", true);
+            dataTable.Rows.Add(8, 8, 4, "good", true);
+            dataTable.Rows.Add(9, 9, 1, "worst", true);
+            dataTable.Rows.Add(10,10, 5, "best", true);
+            dataTable.Rows.Add(11, 11, 4, "bad", true);
+            dataTable.Rows.Add(12, 12, 4, "better", true);
+            dataTable.Rows.Add(13, 13, 2, "bad", false);
+            dataTable.Rows.Add(14, 14, 1, "bad", false);
+            dataTable.Rows.Add(15, 15, 5, "best", false);
+            dataTable.Rows.Add(16, 16, 5, "best", true);
+            dataTable.Rows.Add(17, 17, 4, "good", true);
+            dataTable.Rows.Add(18, 18, 4, "good", true);
+            dataTable.Rows.Add(19, 19, 3, "best", true);
+            dataTable.Rows.Add(20, 20, 3, "bad", true);
+            dataTable.Rows.Add(21, 21, 2, "good", true);
+            dataTable.Rows.Add(22, 22, 5, "best", true);
+            dataTable.Rows.Add(23, 23, 4, "bad", true);
+            dataTable.Rows.Add(24, 24, 3, "good", true);
+            dataTable.Rows.Add(25, 25, 2, "better", true);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine($"{row["ProductId"]}\t|{row["UserId"]}\t|{row["Review"]}\t|{row["Rating"]}\t|{row["IsLike"]}");
+            }
+            return dataTable.ToString();
+        }
+    }
+}
---
+++ UC8Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewSystem
+{
+    public class UC8Program
+    {
+        public static void Main()
+        {
+            UC8ProductHandler.CreateAndInsertToDatabase();
+        }
+    }
+}
